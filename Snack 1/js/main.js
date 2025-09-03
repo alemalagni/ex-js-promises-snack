cf7407d240561e0c4969d24151c8ba68f2e90616
alemalagni <alemalagni@gmail.com>
2025-09-03 13:30:32 +0200

    feat:

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -2,7 +2,7 @@ function getPostTitle(id) {
     return new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
             .then(response => response.json())
-            .then(obj => resolve(obj))
+            .then(obj => resolve(obj.title))
             .catch(reject)
     });
 }
