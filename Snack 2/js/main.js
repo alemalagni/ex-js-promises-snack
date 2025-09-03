d6a3f3e56c1bb6ea4acba4fbf15a1ab8ba888541
alemalagni <alemalagni@gmail.com>
2025-09-03 18:46:25 +0200

    feat: add function

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -1,5 +1,9 @@
 function lanciaDado() {
     return new Promise((resolve, reject) => {
         const incanstro = Math.round(Math.random() * 4)
+
+        if (incanstro > 0) {
+            return (Math.round(Math.random() * 5) + 1)
+        }
     });
 }
