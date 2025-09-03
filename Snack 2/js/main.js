6e241c254d21abe8f980d0351e5a11967087ffcd
alemalagni <alemalagni@gmail.com>
2025-09-03 18:57:46 +0200

    feat

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -1,9 +1,19 @@
 function lanciaDado() {
     return new Promise((resolve, reject) => {
         const incanstro = Math.round(Math.random() * 4)
+        console.log('Sta lanciando il dado...')
 
-        if (incanstro > 0) {
-            return (Math.round(Math.random() * 5) + 1)
-        }
-    });
+        setTimeout(() => {
+            if (incanstro > 0) {
+                const dado = Math.round(Math.random() * 5) + 1;
+                resolve(`È uscito il numero ${dado}`)
+            } else {
+                reject('Il dado si è incastrato')
+            }
+        }, 4000);
+    })
 }
+
+lanciaDado()
+    .then(risultato => console.log(risultato))
+    .catch(error => console.error(error))
